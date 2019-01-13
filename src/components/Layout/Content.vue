4d3dce827b3c7ff85871a2b5bc283d0023d8895b
CHIK SUM <chiksum.wong@gmail.com>
2019-01-14 00:29:34 +0800

    Layout

---
+++ src/components/Layout/Content.vue
@@ -0,0 +1,12 @@
+<template>
+    <b-card class="text-center">
+      This is some text within the default card block.
+    </b-card>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ src/views/Layout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="layout">
+    <Navbar/>
+    <Content/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from "@/components/Layout/Navbar";
+import Content from "@/components/Layout/Content";
+
+export default {
+  name: "layout",
+  components: {
+    Navbar,
+    Content
+  }
+};
+</script>
