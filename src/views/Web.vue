c26a91bc08deebdd93ec31f17fc90e03b659347e
CHIK SUM <chiksum.wong@gmail.com>
2019-01-19 18:12:42 +0800

    Added web page api

--- src/views/Web.vue
+++ src/views/Web.vue
@@ -1,18 +1,49 @@
 <template>
   <div class="container">
+    <br>
     <b-card>
       <b-input-group prepend="Website">
-        <b-form-input></b-form-input>
+        <b-form-input v-model="url"></b-form-input>
         <b-input-group-append>
-          <b-btn variant="info">Submit</b-btn>
+          <b-btn variant="info" @click="onSubmit">Submit</b-btn>
         </b-input-group-append>
       </b-input-group>
+
+      {{result}}
     </b-card>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      url: "",
+      result:"",
+    };
+  },
+  methods: {
+
+    onSubmit() {
+      this.checkNewsByUrl();
+    },
+
+    checkNewsByUrl(){
+      const params = {
+        url: this.url,
+      };
+      this.axios.post('http://127.0.0.1:8000/api/checknewsbyurl/', {
+        params
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    }
+  }
+};
 </script>
 
 <style>
